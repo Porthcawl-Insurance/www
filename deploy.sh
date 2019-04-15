e5d4798cba841c7f10eb2bbfc8257d2d46faf933
Halie <halie.koehler@cyberfortress.com>
2019-04-15 14:00:38 -0500

    project name changed

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gcloud config configurations activate default
-gcloud config set project cyberfortress-www
+gcloud config set project cyberfortress-www-sandbox
 
 hugo -D
 gcloud -q app deploy
