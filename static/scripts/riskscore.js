1dbf78ce43281a903b6383b042b889bdeffbdd7f
Halie <halie.koehler@cyberfortress.com>
2019-05-25 19:26:21 -0500

    Categroizing by tags on score page

--- static/scripts/riskscore.js
+++ static/scripts/riskscore.js
@@ -2,8 +2,6 @@
 
 $(function() {
 
-  var div = $('#score-div');
-
   var data = {
     // id: 'f58f0b44-7b20-5fed-b0aa-dc353b89c630', //CyberFortress
     id: 'b1049048-ef36-5d15-85e3-33f1c6dd3518' //Codeup
@@ -15,7 +13,7 @@ $(function() {
     contentType: "application/json",
     data: JSON.stringify(data),
   }).done(function(response) {
-    console.log (response);
+    console.log(response);
     populate(response);
   }).fail(function(e) {
     console.log(e)
@@ -26,7 +24,7 @@ $(function() {
 
     var domain = result.domain,
       score = result.score,
-      date = new Date (result.model.ts*1000),
+      date = new Date(result.model.ts * 1000),
       conts = result.top_contributions;
 
     $('#domain').text(domain);
@@ -35,7 +33,12 @@ $(function() {
 
     fillRook(score);
     topFactors(conts);
+    categorizeFactors(conts);
     popSummary(conts);
+
+    var factorGrp = $('.factor-grp'),
+        factor = $('p.factor');
+
   }
 
   // color rook based on score
@@ -79,21 +82,25 @@ $(function() {
     var top = [conts[0], conts[1], conts[2]];
     var bottom = getBottomFactors(conts);
 
-    $(top).each(function() {
+    $(top).each(function(i) {
       var name = this.name,
         tag = this.tag,
         desc = this.description,
         summary = this.summary,
         icon = getIcon(tag),
-        modId;
+        collapseId = i + 't-collapse';
 
       $('#top-factors').append(
         '<div class="d-flex flex-row flex-nowrap align-items-center">' +
         '<div class="icon-div"><p class="icon good"><i class="' + icon + '"></i></p></div>' +
         '<div class="text-div">' +
-        '<p class="f-name">' + name + '</p>'+
-        '<p class="f-desc">' + desc +'</p>' +
-        '</div></div>'
+        '<p class="f-name">' + name + ' <span>[ ' + tag + ' ]</span>' +
+        '<a class="expand-icon" href="#' + collapseId + '" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="' + collapseId + '">' +
+        '<i class="fas fa-plus-circle"></i>' +
+        '</a></p>' +
+        '<div id="' + collapseId + '" class="collapse">' +
+        '<p class="f-desc">' + desc + '</p>' +
+        '</div></div></div>'
       );
 
       $('#top-div').append(
@@ -106,30 +113,36 @@ $(function() {
       );
     });
 
-    $(bottom).each(function (i) {
+    $(bottom).each(function(i) {
+
       var name = this[0].name,
         tag = this[0].tag,
         desc = this[0].description,
         summary = this[0].summary,
         icon = getIcon(tag),
-        modId;
+        collapseId = i + 'b-collapse';
+
 
       $('#bottom-factors').append(
         '<div class="d-flex flex-row flex-nowrap align-items-center">' +
         '<div class="icon-div"><p class="icon bad"><i class="' + icon + '"></i></p></div>' +
         '<div class="text-div">' +
-        '<p class="f-name">' + name + '</p>'+
-        '<p class="f-desc">' + desc +'</p>' +
-        '</div></div>'
+        '<p class="f-name">' + name + ' <span>[ ' + tag + ' ]</span>' +
+        '<a class="expand-icon float-right" href="#' + collapseId + '" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="' + collapseId + '">' +
+        '<i class="fas fa-plus-circle"></i>' +
+        '</a></p>' +
+        '<div id="' + collapseId + '" class="collapse">' +
+        '<p class="f-desc">' + desc + '</p>' +
+        '</div></div></div>'
       );
     });
 
     function getBottomFactors(conts) {
       var clone = conts.slice(0), //clone array as to not change orignal conts[]
-          lastConts = [],
-          step;
+        lastConts = [],
+        step;
 
-      for (step = 0; step < 3; step++){ // get .last(), then .pop() from clone, 3x
+      for (step = 0; step < 3; step++) { // get .last(), then .pop() from clone, 3x
         var lCont = $(clone).last();
         lastConts.push(lCont);
         clone.pop(lCont);
@@ -139,6 +152,32 @@ $(function() {
       return lastConts;
     }
 
+  }
+
+  // categorize factors by tag
+  function categorizeFactors(conts) {
+    var factorGrps = $('.factor-grp'),
+        factorList = $('.factor-list');
+
+    $(conts).each(function() {
+      var tag = this.tag;
+
+      if (tag === 'web-server') {
+        tag = 'server';
+      }
+
+      $('#rs-' + tag).append(
+        '<p class="rs-factor">' + this.name + '</p>'
+      );
+
+    });
+
+    // if factor group has nothing listed, hide it
+    $(factorGrps).each(function(i) {
+      if ($(this).children().length <= 2) {
+        $(this).hide();
+      }
+    });
   }
 
   // create summary
@@ -208,8 +247,8 @@ $(function() {
         }
 
         if (summary.indexOf('positive') >= 0) {
-            color = 'r7';
-        } else if (summary.indexOf('negative') >= 0){
+          color = 'r7';
+        } else if (summary.indexOf('negative') >= 0) {
           color = 'r1';
         }
 
@@ -266,18 +305,18 @@ $(function() {
       case 'mx':
         icon = 'fal fa-fingerprint';
         break;
-      case 'mod_cms':
-        icon = 'fal fa-cog';
+      case 'server' || 'web-server':
+        icon = 'fal fa-server';
         break;
-      case 'mod_ns':
-        icon = 'fal fa-fingerprint';
-        break;
-      case 'mod_widgets':
-        icon = 'fal fa-puzzle-piece';
+      case 'payment':
+        icon = 'fal fa-money-bill-wave'
         break;
     }
     return icon;
   }
+
+  // color by weight
+
 
   // scroll to full summary btn function
   $('#full-summary').click(function() {
@@ -285,158 +324,5 @@ $(function() {
       scrollTop: $("#summary-div").offset().top
     }, 1000);
   });
-
-
-  $('.factor-exp').click(function() {
-    console.log('clicked!');
-  });
-
-
-
-
-  // $(div).ready(function() {
-  //
-  //   // data returned from api
-  //   $.post("https://us-central1-cyberfortress-www.cloudfunctions.net/demo_risk_score", function(data) {
-  //     console.log(data);
-  //
-  //     var summary = data.summary,
-  //       // score = data.score,
-  //       score = 1,
-  //       email = data.domain,
-  //       rook = $('#rook-svg'),
-  //       color;
-  //
-  //     $('#domain').text(email);
-  //     $('#score').html('<span>' + score + '</span> / 10 ');
-  //
-  //
-  //     fillRook();
-  //     scoreMessage();
-  //     populateCats();
-  //
-  //     // //chose color for rook based on score
-  //     // $('#circle').css({fill: "red", transition: "2.0s"});
-  //
-  //
-  //     function fillRook() {
-  //       switch (true) {
-  //         case (score <= 1.42):
-  //           color = '#cc444b';
-  //           // $('.r1').css('fill', color);
-  //           $('.r1').css({fill: color, transition: "2.0s"});
-  //           break;
-  //         case (score <= 2.83):
-  //           color = '#d8774f';
-  //           $('.r1, .r2').css('fill', color);
-  //           break;
-  //         case (score <= 4.28):
-  //           color = '#de9151';
-  //           $('.r1, .r2, .r3').css('fill', color);
-  //           break;
-  //         case (score <= 5.71):
-  //           color = '#e9b15d';
-  //           $('.r1, .r2, .r3, .r4').css('fill', color);
-  //           break;
-  //         case (score <= 7.14):
-  //           color = '#fff275';
-  //           $('.r1, .r2, .r3, .r4, .r5').css('fill', color);
-  //           break;
-  //         case (score <= 8.57):
-  //           color = '#d3e468';
-  //           $('.r1, .r2, .r3, .r4, .r5, .r6').css('fill', color);
-  //           break;
-  //         case (score <= 10):
-  //           color = '#7ac74f';
-  //           $('.r1, .r2, .r3, .r4, .r5, .r6, .r7').css('fill', color);
-  //           break;
-  //       }
-  //     }
-  //
-  //     function scoreMessage() {
-  //
-  //       var input = $('#score-message');
-  //
-  //       if(score <= 3.5){
-  //         input.text('Yikes');
-  //       } else if (score <= 7){
-  //         input.text('Not bad');
-  //       } else if (score <= 10){
-  //         input.text('Good job! Thats a great score.')
-  //       }
-  //     }
-  //
-  //     function populateCats() {
-  //       // populate summary from json
-  //       $(summary).each(function() {
-  //         var alarm = this.alarm,
-  //         category = this.category,
-  //         description = this.description,
-  //         links = this.links,
-  //         notes = this.notes,
-  //         icon,
-  //         color,
-  //         note;
-  //
-  //         // icons for each category
-  //         switch (category) {
-  //           case 'Email and Identity':
-  //             icon = 'fas fa-fingerprint';
-  //             break;
-  //           case 'Hosting Provider':
-  //             icon = 'fas fa-cloud';
-  //             break;
-  //           case 'DNS':
-  //             icon = 'fas fa-globe';
-  //             break;
-  //           case 'Web Servers':
-  //             icon = 'fas fa-server';
-  //             break;
-  //           case 'TLS/SSL':
-  //             icon = 'fas fa-shield-alt';
-  //             break;
-  //           case 'CMS':
-  //             icon = 'fas fa-cog';
-  //             break;
-  //           case 'Footprint':
-  //             icon = 'fas fa-shoe-prints';
-  //             break;
-  //         }
-  //
-  //         // icon colors based on alarm
-  //         switch (alarm) {
-  //           case 1:
-  //             color = 'r7';
-  //             break;
-  //           case 0:
-  //             color = 'r4';
-  //             break;
-  //           case -1:
-  //             color = 'r1';
-  //             break;
-  //         }
-  //
-  //         $('#cat-div').append(
-  //           "<div class='cat'><p class='icon " +
-  //           color + "'><i class='" +
-  //           icon +"'></i></p><p class='c-title'>" +
-  //           category + "</p><p class='c-text'>" +
-  //           description + "</p></div>");
-  //       });
-  //     }
-  //
-  //     function randomNum(){
-  //       score = Math.floor((Math.random() * 10) + 1);
-  //       return;
-  //     }
-  //   });
-  //
-  // });
-  //
-  // var data = {
-  //   id : "f58f0b44-7b20-5fed-b0aa-dc353b89c630"
-  // };
-
-
 
 });
