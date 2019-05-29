55c3508210b348770ce7f011a30f6c794493d28b
Halie <halie.koehler@cyberfortress.com>
2019-05-29 16:37:16 -0500

    basic form validation with bs4

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,6 +2,27 @@
 
 $(function() {
 
+
+  // Intercom Chat //
+  $('.intercom-launcher-open-icon').css('backgroundColor', 'black');
+
+  // Team Photos //
+  $('.figure-img').click(function() {
+    // Go to LinkedIn Page on click
+    var link = $(this).siblings('.figure-caption').find('a').attr('href');
+    window.location.href = link;
+    // window.open(link);
+  });
+
+  $('.figure-img').hover(function() {
+    var link = $(this).siblings('.figure-caption').find('a');
+    $(link).css('fontWeight', 'bold');
+  }, function() {
+    var link = $(this).siblings('.figure-caption').find('a');
+    $(link).css('fontWeight', '400');
+  });
+
+
   // ------ Handle Footer Height -------//
   $(document).ready(function() {
 
