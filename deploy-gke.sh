0962d46067c51053af3cde67613c9f935c7bb082
Michael T. DeFelice <michael.defelice@gmail.com>
2019-06-04 15:07:46 -0500

    Updated GKE deployment

--- deploy-gke.sh
+++ deploy-gke.sh
@@ -1,28 +1,65 @@
 #!/bin/bash
 
+# Assume default credentials but not the current project
 gcloud config configurations activate default
 
-GCP_PROJECT=cyberfortress-sandbox
+GCP_PROJECT=cyberfortress-www
 gcloud config set project $GCP_PROJECT
 
+IMAGE_NAME=cf-www
+IMAGE_PATH=gcr.io/$GCP_PROJECT/$IMAGE_NAME
+
+# Rebuild site
 hugo -D
 
 # Docker
-docker build -t cf-www:latest .
-docker tag cf-www:latest gcr.io/$GCP_PROJECT/cf-www:latest
-docker push gcr.io/$GCP_PROJECT/cf-www:latest
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+docker build -t ${IMAGE_NAME}:latest .
+docker tag ${IMAGE_NAME}:latest ${IMAGE_PATH}:latest
+docker push ${IMAGE_PATH}:latest
 
-# k8s initial
-# kubectl run cf-www --image=gcr.io/$GCP_PROJECT/cf-www --port=80
-# kubectl expose deployment cf-www --target-port=80 --type=NodePort
+# Remove dangling images if necessary
+# docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
+# k8s
+gcloud container clusters get-credentials www --region us-central1-a
+
+# Create deployment
+# cat << EOF | kubectl apply -f -
+# apiVersion: apps/v1
+# kind: Deployment
+# metadata:
+#   name: $IMAGE_NAME
+#   labels:
+#     app: $IMAGE_NAME
+# spec:
+#   replicas: 2
+#   selector:
+#     matchLabels:
+#       app: $IMAGE_NAME
+#   template:
+#     metadata:
+#       labels:
+#         app: $IMAGE_NAME
+#     spec:
+#       containers:
+#       - name: $IMAGE_NAME
+#         image: $IMAGE_PATH
+#         ports:
+#         - containerPort: 80
+# 
+# EOF
+
+# Create service
+# kubectl expose deployment $IMAGE_NAME --target-port=80 --type=NodePort
+
+# Create ingress ... note the hard-coded "www-static" address - this must exist
 # cat << EOF | kubectl apply -f -
 # apiVersion: extensions/v1beta1
 # kind: Ingress
 # metadata:
-#   name: cf-www-ingress
+#   name: ${IMAGE_NAME}-ingress
 #   annotations:
-#     kubernetes.io/ingress.global-static-ip-name: "cf-sandbox"
+#     kubernetes.io/ingress.global-static-ip-name: "www-static"
 # spec:
 #   backend:
 #     serviceName: cf-www
@@ -30,7 +67,7 @@ docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 # 
 # EOF
 
-# k8s
-kubectl scale --replicas=0 deployment/cf-www
-kubectl scale --replicas=1 deployment/cf-www
+# Scale
+kubectl scale --replicas=0 deployment/$IMAGE_NAME
+kubectl scale --replicas=2 deployment/$IMAGE_NAME
 
